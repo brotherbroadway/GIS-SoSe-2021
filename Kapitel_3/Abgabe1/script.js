42e825a287c2720e887d5cf25f68647fe3e98155
Jona <saibot.jona@gmail.com>
2021-05-30 01:22:34 +0200

    Updated for FormData

--- Kapitel_3/Abgabe1/script.js
+++ Kapitel_3/Abgabe1/script.js
@@ -1,2 +1,25 @@
 "use strict";
+var Abgabe3_1;
+(function (Abgabe3_1) {
+    async function sendData() {
+        let dataForm = new FormData(document.forms[0]);
+        console.log(": " + dataForm.get("name"));
+        for (let entry of dataForm) {
+            console.log(entry);
+            console.log("name:" + entry[0]);
+            console.log("value:" + entry[1]);
+        }
+        let query = new URLSearchParams(dataForm);
+        let _url = "https://superomegaepicapp.herokuapp.com";
+        _url = _url + "?" + query.toString();
+        console.log(_url);
+        let dataResponse = await fetch(_url);
+        let dataReply = await dataResponse.text();
+        console.log(dataReply);
+        let dataLog = document.getElementById("serverReply");
+        dataLog.innerText = dataReply;
+    }
+    let buttonSend = document.getElementById("sendButton");
+    buttonSend.addEventListener("click", sendData);
+})(Abgabe3_1 || (Abgabe3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel_3/Abgabe1/script.ts
+++ Kapitel_3/Abgabe1/script.ts
@@ -1,3 +1,26 @@
 namespace Abgabe3_1 {
-    
+
+    async function sendData(): Promise <void> {
+        let dataForm: FormData = new FormData(document.forms[0]);
+        console.log(": " + dataForm.get("name"));
+
+        for (let entry of dataForm) {
+            console.log(entry);
+            console.log("name:" + entry[0]);
+            console.log("value:" + entry[1]);
+        }
+
+        let query: URLSearchParams = new URLSearchParams(<any>dataForm);
+        let _url: RequestInfo = "https://superomegaepicapp.herokuapp.com";
+        _url = _url + "?" + query.toString();
+        console.log(_url);
+        let dataResponse: Response = await fetch(_url);
+        let dataReply: string = await dataResponse.text();
+        console.log(dataReply);
+        let dataLog: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverReply");
+        dataLog.innerText = dataReply;
+    }
+
+    let buttonSend: HTMLButtonElement = <HTMLButtonElement> document.getElementById("sendButton");
+    buttonSend.addEventListener("click", sendData);
 }
