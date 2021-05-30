ac7c93815abd1d14a40c27111b2b5681cadb0339
Jona <saibot.jona@gmail.com>
2021-05-30 16:03:22 +0200

    Updated FormData

--- Kapitel_3/Abgabe1/script.js
+++ Kapitel_3/Abgabe1/script.js
@@ -3,20 +3,20 @@ var Abgabe3_1;
 (function (Abgabe3_1) {
     async function sendData() {
         let dataForm = new FormData(document.forms[0]);
+        let query = new URLSearchParams(dataForm);
+        let _url = "https://superomegaepicapp.herokuapp.com";
+        let dataResponse = await fetch(_url);
+        let dataReply = await dataResponse.text();
+        let dataLog = document.getElementById("serverReply");
         console.log(": " + dataForm.get("name"));
         for (let entry of dataForm) {
             console.log(entry);
             console.log("name:" + entry[0]);
             console.log("value:" + entry[1]);
         }
-        let query = new URLSearchParams(dataForm);
-        let _url = "https://superomegaepicapp.herokuapp.com";
         _url = _url + "?" + query.toString();
         console.log(_url);
-        let dataResponse = await fetch(_url);
-        let dataReply = await dataResponse.text();
         console.log(dataReply);
-        let dataLog = document.getElementById("serverReply");
         dataLog.innerText = dataReply;
     }
     let buttonSend = document.getElementById("sendButton");
--- Kapitel_3/Abgabe1/script.ts
+++ Kapitel_3/Abgabe1/script.ts
@@ -2,6 +2,11 @@ namespace Abgabe3_1 {
 
     async function sendData(): Promise <void> {
         let dataForm: FormData = new FormData(document.forms[0]);
+        let query: URLSearchParams = new URLSearchParams(<any>dataForm);
+        let _url: RequestInfo = "https://superomegaepicapp.herokuapp.com";
+        let dataResponse: Response = await fetch(_url);
+        let dataReply: string = await dataResponse.text();
+        let dataLog: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverReply");
         console.log(": " + dataForm.get("name"));
 
         for (let entry of dataForm) {
@@ -9,15 +14,10 @@ namespace Abgabe3_1 {
             console.log("name:" + entry[0]);
             console.log("value:" + entry[1]);
         }
-
-        let query: URLSearchParams = new URLSearchParams(<any>dataForm);
-        let _url: RequestInfo = "https://superomegaepicapp.herokuapp.com";
+        
         _url = _url + "?" + query.toString();
         console.log(_url);
-        let dataResponse: Response = await fetch(_url);
-        let dataReply: string = await dataResponse.text();
         console.log(dataReply);
-        let dataLog: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverReply");
         dataLog.innerText = dataReply;
     }
 
