3ada56b40f4a3334d44f387c5c639da0c99362fc
Jona <saibot.jona@gmail.com>
2021-06-02 17:42:04 +0200

    Updated for proper server response

--- Kapitel_3/Abgabe1/script.js
+++ Kapitel_3/Abgabe1/script.js
@@ -1,25 +1,36 @@
 "use strict";
 var Abgabe3_1;
 (function (Abgabe3_1) {
-    async function sendData() {
-        let dataForm = new FormData(document.forms[0]);
+    async function sendData(_url) {
+        let dataForm = new FormData(document.forms[0]); // defines form
         let query = new URLSearchParams(dataForm);
-        let _url = "https://superomegaepicapp.herokuapp.com";
+        _url = _url + "?" + query.toString();
         let dataResponse = await fetch(_url);
         let dataReply = await dataResponse.text();
         let dataLog = document.getElementById("serverReply");
+        let dataArea = document.createElement("textarea");
+        let stringOutput = "";
         console.log(": " + dataForm.get("name"));
-        for (let entry of dataForm) {
+        for (let entry of dataForm) { // for each form entry, logs form in string & console
+            stringOutput += "Name: " + entry[0] + "\nValue: " + entry[1] + "\n\n";
             console.log(entry);
             console.log("name:" + entry[0]);
             console.log("value:" + entry[1]);
         }
-        _url = _url + "?" + query.toString();
         console.log(_url);
         console.log(dataReply);
-        dataLog.innerText = dataReply;
+        dataArea.appendChild(document.createTextNode(stringOutput)); // adds form string to textarea element
+        dataArea.appendChild(document.createTextNode("URL: " + dataReply));
+        if (dataLog.hasChildNodes() == false) { // checks if there's already a textarea, but also has to refresh for another entry
+            dataLog.appendChild(dataArea); // adds textarea element to div
+            dataArea.setAttribute("rows", "20");
+            dataArea.setAttribute("cols", "35");
+        }
     }
-    let buttonSend = document.getElementById("sendButton");
-    buttonSend.addEventListener("click", sendData);
+    function sendButton(_event) {
+        sendData("https://superomegaepicapp.herokuapp.com");
+    }
+    let buttonSend = document.getElementById("sendButton"); // button defined
+    buttonSend.addEventListener("click", sendButton);
 })(Abgabe3_1 || (Abgabe3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Kapitel_3/Abgabe1/script.ts
+++ Kapitel_3/Abgabe1/script.ts
@@ -1,26 +1,38 @@
 namespace Abgabe3_1 {
 
-    async function sendData(): Promise <void> {
-        let dataForm: FormData = new FormData(document.forms[0]);
+    async function sendData(_url: RequestInfo): Promise <void> {
+        let dataForm: FormData = new FormData(document.forms[0]); // defines form
         let query: URLSearchParams = new URLSearchParams(<any>dataForm);
-        let _url: RequestInfo = "https://superomegaepicapp.herokuapp.com";
+        _url = _url + "?" + query.toString();
         let dataResponse: Response = await fetch(_url);
         let dataReply: string = await dataResponse.text();
-        let dataLog: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverReply");
+        let dataLog: HTMLDivElement = <HTMLDivElement> document.getElementById("serverReply");
+        let dataArea: HTMLTextAreaElement = document.createElement("textarea");
+        let stringOutput: string = "";
         console.log(": " + dataForm.get("name"));
 
-        for (let entry of dataForm) {
+        for (let entry of dataForm) { // for each form entry, logs form in string & console
+            stringOutput += "Name: " + entry[0] + "\nValue: " + entry[1] + "\n\n";
             console.log(entry);
             console.log("name:" + entry[0]);
             console.log("value:" + entry[1]);
         }
-        
-        _url = _url + "?" + query.toString();
+
         console.log(_url);
         console.log(dataReply);
-        dataLog.innerText = dataReply;
+        dataArea.appendChild(document.createTextNode(stringOutput)); // adds form string to textarea element
+        dataArea.appendChild(document.createTextNode("URL: " + dataReply));
+        if (dataLog.hasChildNodes() == false) { // checks if there's already a textarea, but also has to refresh for another entry
+            dataLog.appendChild(dataArea); // adds textarea element to div
+            dataArea.setAttribute("rows", "20");
+            dataArea.setAttribute("cols", "35");
+        }
     }
 
-    let buttonSend: HTMLButtonElement = <HTMLButtonElement> document.getElementById("sendButton");
-    buttonSend.addEventListener("click", sendData);
+    function sendButton(_event: Event): void { // establishes communication with set website on click
+        sendData("https://superomegaepicapp.herokuapp.com");
+    }
+
+    let buttonSend: HTMLButtonElement = <HTMLButtonElement> document.getElementById("sendButton"); // button defined
+    buttonSend.addEventListener("click", sendButton);
 }
