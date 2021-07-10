553adfcf0d66bd8cdfbd2a7dd85af9ed958f2171
Jona <saibot.jona@gmail.com>
2021-07-11 00:25:01 +0200

    Created for better management

---
+++ interface.d.ts
@@ -0,0 +1,19 @@
+interface RecipeForm { // Recipe form entry as interface
+    recipeName: string;
+    ingredient1: string;
+    ingredient2: string;
+    ingredient3: string;
+    ingredient4: string;
+    ingredient5: string;
+    ingredient6: string;
+    ingredient7: string;
+    ingredient8: string;
+    recipeNotes: string;
+    recipeAuthor: string;
+}
+
+interface UserRegForm { // User form entry as interface
+    username: string;
+    password: string;
+    favRecipes: RecipeForm[];
+}
