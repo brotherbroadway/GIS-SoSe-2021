a46f2907d50fff38bbc792f710c176b0626f8eee
Jona <saibot.jona@gmail.com>
2021-07-10 19:43:31 +0200

    Created script for allRecipes

--- Endabgabe/client/script_recipes.js
+++ Endabgabe/client/script_recipes.js
@@ -1,2 +1,24 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AbgabeEnd = void 0;
+var AbgabeEnd;
+(function (AbgabeEnd) {
+    // let sendToURL: string = "http://localhost:8100"; // private testing 009
+    window.addEventListener("load", loadAllRecipes);
+    let _url;
+    function urlHere() {
+        _url = "https://superomegaepicapp.herokuapp.com"; // public testing
+        // _url = "http://localhost:8100"; // private testing
+    }
+    async function loadAllRecipes() {
+        urlHere();
+        _url += "/recipesAll?";
+        console.log(urlHere);
+        let allResponse = await fetch(_url);
+        let allReply = await allResponse.json();
+        let loggedinUser = sessionStorage.getItem("ssnUser");
+        console.log("Logged in user:" + loggedinUser);
+        console.log(allReply);
+    }
+})(AbgabeEnd = exports.AbgabeEnd || (exports.AbgabeEnd = {}));
 //# sourceMappingURL=script_recipes.js.map
--- Endabgabe/client/script_recipes.ts
+++ Endabgabe/client/script_recipes.ts
@@ -1,3 +1,26 @@
-namespace AbgabeEnd {
-    
+import * as Interface from "../server/interface";
+
+export namespace AbgabeEnd {
+    // let sendToURL: string = "http://localhost:8100"; // private testing 009
+
+    window.addEventListener("load", loadAllRecipes);
+
+    let _url: string;
+
+    function urlHere(): void {
+        _url = "https://superomegaepicapp.herokuapp.com"; // public testing
+        // _url = "http://localhost:8100"; // private testing
+    }
+
+    async function loadAllRecipes(): Promise <void> {
+        urlHere();
+        _url += "/recipesAll?";
+        console.log(urlHere);
+
+        let allResponse: Response = await fetch(_url);
+        let allReply: Interface.RecipeForm[] = await allResponse.json();
+        let loggedinUser: string = sessionStorage.getItem("ssnUser");
+        console.log("Logged in user:" + loggedinUser);
+        console.log(allReply);
+    }
 }
