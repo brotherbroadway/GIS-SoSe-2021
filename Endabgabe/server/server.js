283c2ea70fb3e453cb595c5f015b0a91b7a5156f
Jona <saibot.jona@gmail.com>
2021-07-11 06:41:26 +0200

    Updated favoritism

--- Endabgabe/server/server.js
+++ Endabgabe/server/server.js
@@ -123,7 +123,7 @@ var AbgabeEnd;
                 let allFavs = new Array();
                 let userReg = await dbUserCollection.findOne({ "username": myURL.query.crntUser });
                 // to check if recipe is already fav'd
-                let dbRecipeCheck = await dbUserCollection.find({ "username": myURL.query.crntUser.toString(), "favRecipes": myURL.query._id.toString() }).limit(1).count(true);
+                let dbRecipeCheck = await dbUserCollection.find({ "username": myURL.query.crntUser.toString(), "favRecipes": [myURL.query._id.toString()] }).limit(1).count(true);
                 if (dbRecipeCheck == 1) {
                     _response.write("FailFav");
                 }
